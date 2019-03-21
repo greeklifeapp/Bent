2c2dcc3b7065e49010ed038aa9bab5e316238b6e
Terry <terry.wade@valpo.edu>
2019-03-21 15:51:24 -0500

    Initialize Login page and firebase

---
+++ src/pages/login/login.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { GlobalProvider } from '../../providers/global/global';
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public global: GlobalProvider) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+}
--- src/providers/global/global.ts
+++ src/providers/global/global.ts
@@ -1,17 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import firebase from 'firebase';
 
-/*
-  Generated class for the GlobalProvider provider.
+declare var gapi: any;
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class GlobalProvider {
 
   constructor(public http: HttpClient) {
-    console.log('Hello GlobalProvider Provider');
+    const config = {
+      apiKey: "AIzaSyDNs1AeJaJ7_yrZrlqaBc6UY4DTzKCzxMw",
+      authDomain: "testingjs-6c353.firebaseapp.com",
+      databaseURL: "https://testingjs-6c353.firebaseio.com",
+      projectId: "testingjs-6c353",
+      storageBucket: "testingjs-6c353.appspot.com",
+      messagingSenderId: "164663954733"
+    };
+    firebase.initializeApp(config);
+
+    gapi.load('client:auth2', () => {
+      gapi.client.init({
+        apiKey: "AIzaSyADDwygDKqWmMNq8XAaz7gvqYh5lEAMRgc",
+        clientId: "111558558514-opgcs0nmd9oeqtv2mn0pa0q3s766g444.apps.googleusercontent.com",
+        scope: "https://www.googleapis.com/auth/calendar",
+      })
+    })
+
+    console.log('he')
   }
 
 }
